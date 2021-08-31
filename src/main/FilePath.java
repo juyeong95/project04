5b69f61d3926ac8dc730490e08f8c142c5792721
juyeong <bn0406@naver.com>
2021-08-31 20:11:43 +0900

    메인 프로젝트 생성

---
+++ src/main/FilePath.java
@@ -0,0 +1,9 @@
+package main;
+
+public class FilePath {
+	public static String path1 = "";
+	public static String path2 = "";
+	public static String path3 = "";
+	public static String path4 = "";
+	public static String path5 = "";
+}
---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.이주영님 2.성기룡님 3.설지희님 4.서광훈님 5.송영관님 6.종료");
+			num=input.nextInt();
+			switch(num) {
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			case 6: System.exit(0);
+			}
+		}
+	}
+}
