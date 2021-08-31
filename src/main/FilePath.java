e624c8f72496e9411e49a9c05fb5ac9ac3d9fa65
seoljihee <wlgml7997@naver.com>
2021-08-31 20:27:23 +0900

    3 경로 입력 완료

--- src/main/FilePath.java
+++ src/main/FilePath.java
@@ -3,7 +3,7 @@ package main;
 public class FilePath {
 	public static String path1 = "";
 	public static String path2 = "";
-	public static String path3 = "";
+	public static String path3 = "C:/Users/Admin/Desktop/java project/git-workspace/filePath/project04/3";
 	public static String path4 = "";
 	public static String path5 = "";
 }
